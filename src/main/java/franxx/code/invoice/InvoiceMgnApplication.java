49165b67bbd0c77dfe9f33bfe4220b11a84f305e
Hilmi AM <imiia75775@proton.me>
2024-07-20 12:18:04 +0700

    init

---
+++ src/main/java/franxx/code/invoice/InvoiceMgnApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMgnApplication.class, args);
+	}
+
+}
